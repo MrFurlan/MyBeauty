d498e4d6e1a297b7e2330af14fd5ca032c77806b
MrFurlan <mrfurlan10@hotmail.com>
2017-09-19 16:30:15 -0300

    ajustes na solucao

---
+++ MyBeauty/App_Start/BundleConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBeauty.App_Start
+{
+    public class BundleConfig
+    {
+    }
+}
---
+++ MyBeauty/Models/EstabelecimentoXColaboradorXServico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBeauty.Models
+{
+    public class EstabelecimentoXColaboradorXServico
+    {
+        public EstabelecimentoXColaborador EstabelecimentoXColaborador { get; set; }
+        public Servico Servico { get; set; }
+        public double Valor { get; set; }
+        public double ValorPromocional { get; set; }
+        public double PercentualDesconto { get; set; }
+    }
+}
